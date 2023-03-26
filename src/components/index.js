e0de511b7112a4019aea11dc17062d940ee59f99
RakshithBhat03 <rakshith6666@gmail.com>
2023-03-26 19:47:12 +0530

    [chore]: Add/Update exports

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { Captcha } from "./Captcha/Captcha";
+export { Tile } from "./Tile/Tile";
+export { FormCompletion } from "./FormCompletion/FormCompletion";
+export { Form } from "./Form/Form";
